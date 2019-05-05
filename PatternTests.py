4c47d094b7989d28b3dd1a9473cb6242b8cfbf10
loisgh <loisgh@nyc.rr.com>
2019-05-05 09:34:54 -0400

    Finish writing test methods

--- PatternTests.py
+++ PatternTests.py
@@ -34,25 +34,39 @@ class PatternTests(unittest.TestCase):
         self.assertEqual(expected_wildcard_count, wildcard_count)
 
     def test_match_the_line(self):
+        regexes = \
+            {'*,*,c': {'idx_sum': 1, 'regex': '^.[/].[/][c]$', 'wildcard_count': 2},
+             '*,b,*': {'idx_sum': 2, 'regex': '^.[/][b][/].$', 'wildcard_count': 2},
+             '*,x,y,z': {'idx_sum': 0, 'regex': '^.[/][x][/][y][/][z]$', 'wildcard_count': 1},
+             'a,*,*': {'idx_sum': 3, 'regex': '^[a][/].[/].$', 'wildcard_count': 2},
+             'foo,bar,baz': {'idx_sum': 0, 'regex': '^foo[/]bar[/]baz$', 'wildcard_count': 0},
+             'w,x,*,*': {'idx_sum': 5, 'regex': '^[w][/][x][/].[/].$', 'wildcard_count': 2}}
+
         patterns =  \
             {5: {'*': ['*,b,*', '*,*,c'], 'a': ['a,*,*']},
             7: {'*': ['*,x,y,z'], 'w': ['w,x,*,*']},
             11: {'f': ['foo,bar,baz']}}
 
-        regexes = \
-            {'*,*,c': {'idx_sum': 1, 'regex': '^.[/].[/][c]$', 'wildcard_count': 2},
-            '*,b,*': {'idx_sum': 2, 'regex': '^.[/][b][/].$', 'wildcard_count': 2},
-            '*,x,y,z': {'idx_sum': 0, 'regex': '^.[/][x][/][y][/][z]$', 'wildcard_count': 1},
-            'a,*,*': {'idx_sum': 3, 'regex': '^[a][/].[/].$', 'wildcard_count': 2},
-            'foo,bar,baz': {'idx_sum': 0,'regex': '^foo[/]bar[/]baz$','wildcard_count': 0},
-            'w,x,*,*': {'idx_sum': 5, 'regex': '^[w][/][x][/].[/].$', 'wildcard_count': 2}}
-
-    def test_determine_correct_regex(self):
-        pass
-
-    def test_match_paths(self):
-        pass
+        expected_result = 'w,x,*,*'
+        pat = Pattern()
+        actual_result = pat.match_the_line('/w/x/y/z/', patterns, regexes)
+        self.assertEqual(expected_result, actual_result)
 
     def test_process_pattern(self):
-        pass
-    
+        regexes = \
+            {'*,*,c': {'idx_sum': 1, 'regex': '^.[/].[/][c]$', 'wildcard_count': 2},
+             '*,b,*': {'idx_sum': 2, 'regex': '^.[/][b][/].$', 'wildcard_count': 2},
+             '*,x,y,z': {'idx_sum': 0, 'regex': '^.[/][x][/][y][/][z]$', 'wildcard_count': 1},
+             'a,*,*': {'idx_sum': 3, 'regex': '^[a][/].[/].$', 'wildcard_count': 2},
+             'foo,bar,baz': {'idx_sum': 0, 'regex': '^foo[/]bar[/]baz$', 'wildcard_count': 0},
+             'w,x,*,*': {'idx_sum': 5, 'regex': '^[w][/][x][/].[/].$', 'wildcard_count': 2}}
+
+        patterns =  \
+            {5: {'*': ['*,b,*', '*,*,c'], 'a': ['a,*,*']},
+            7: {'*': ['*,x,y,z'], 'w': ['w,x,*,*']},
+            11: {'f': ['foo,bar,baz']}}
+
+        expected_result = 'w,x,*,*'
+        pat = Pattern()
+        actual_result = pat.match_paths('/w/x/y/z/', patterns, regexes)
+        self.assertEqual(expected_result, actual_result)
